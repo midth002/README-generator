e29620bcf59e020c734c8328be9e42248b597db5
Andrew Midthun <midth002@gmail.com>
2022-04-13 18:06:50 -0500

    Added the utils file and created an array called questions. Moved the prompt to the init() function.

--- index.js
+++ index.js
@@ -1,88 +1,89 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkDown = require("./utils/generateMarkdown.js")
 // const license = require('license')
 
 // TODO: Create an array of questions for user input
-function askReadMeQuestions() {
-    inquirer 
-    .prompt([
-        {
-            type: 'input',
-            name: 'username',
-            message: 'What is your GitHub username?'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?'
-        },
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of your project?'
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What kind of license should your project have?',
-            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
-        },
-        {
-            type: 'input',
-            name: 'dependencies',
-            message: 'What command should be run to install dependencies?'
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'What command should be run to run tests?'
-        },
-        {
-            type: 'input',
-            name: 'using',
-            message: 'What does the user need to know about using the repo?'
-        },
-      
-        {
-            type: 'input',
-            name: 'contributing',
-            message: 'What does the user need to know about contributing to the repo?'
-        },
-    ])
-    .then((response) => {
-        console.log(response.license)
-        // const answer = JSON.stringify(response)
-       
-        // console.log(answer.license)
-        // console.log(answer)
-        // writeToFile("README.md", JSON.stringify(response))
-    
-    })
-}
+const questions = [ 'What is your GitHub username?', 
+                    'What is your email address?', 
+                    'What is the title of your project?',
+                    'Provide a description of your project?', 
+                    'What kind of license should your project have?', 
+                    'What command should be run to install dependencies?', 
+                    'What command should be run to run tests?',
+                    'What does the user need to know about using the repo?',
+                    'What does the user need to know about contributing to the repo?'
+]
 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
+    fs.writeFile(fileName, generateMarkDown(data.title, data.license, data.description), (err) => {
         if (err) {
             console.log(err)
-        } else {
-            console.log("Generating README...")
-            console.log(data.license)
-            
-        }
+        } 
     })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    askReadMeQuestions();
+    inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: questions[0]
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[1]
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[3]
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: questions[4],
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+        },
+        {
+            type: 'input',
+            name: 'dependencies',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: questions[6]
+        },
+        {
+            type: 'input',
+            name: 'using',
+            message: questions[7]
+        },
+      
+        {
+            type: 'input',
+            name: 'contributing',
+            message: questions[8]
+        },
+    ])
+    .then((response) => {
+        console.log(JSON.stringify(response.license))
+        
+        console.log(response)
+}
+    );
 }
 
 // Function call to initialize app
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,105 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  switch(license) {
+    case "APACHE 2.0":
+      return "[License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+    case "GPL 3.0": 
+      return "[License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)"
+    case "BSD 3": 
+    return "[License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+    case "MIT": 
+      return "[License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
+    default: 
+      return ""
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch(license) {
+    case "APACHE 2.0":
+      return "https://opensource.org/licenses/Apache-2.0"
+    case "GPL 3.0": 
+      return "https://www.gnu.org/licenses/gpl-3.0"
+    case "BSD 3": 
+    return "https://opensource.org/licenses/BSD-3-Clause"
+    case "MIT": 
+      return "https://opensource.org/licenses/MIT"
+    default: 
+      return ""
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    if (license === NULL) {
+      return ""
+    } else {
+      return `[!${renderLicenseBadge(license)}]${renderLicenseLink(license)}`
+    } 
+    
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ${renderLicenseSection(data.license)}
+  
+  ## Table of Contents 
+
+  * [Description](#description)
+  
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+  
+  * [License](#license)
+  
+  * [Contributing](#contributing)
+  
+  * [Tests](#tests)
+  
+  * [Questions](#questions)
+
+  ## Description
+
+  ${data.description}
+  
+  ## Installation 
+  
+  To install the necessary dependencies, run the following command:
+  
+  ```
+  
+
+  ```
+  
+  ## Usage
+  
+  ## License 
+  
+  ## Contributing
+  
+  ## Tests
+  
+  To run tests, run the following command: 
+  
+  ``` 
+  
+
+
+  ```
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at [midth002@gmail.com](mailto:midth002@gmail.com)
+  
+  
+`;
+}
+
+module.exports = generateMarkdown;
